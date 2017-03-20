619a3e687414d4517ad191a0e696361559edcdec
Dónal Murray <dmurray654@gmail.com>
2017-03-20 17:02:14 +0000

    Start week 7 assignment. Add constructors and iostream for cartesian class

---
+++ week7/week7.cpp
@@ -0,0 +1,114 @@
+// week7.cpp : Cartesian manipulator
+// Classes to store and manipulate n-dim vectors, 4vectors and particles
+// Author:  Dónal Murray
+// Date:	20/03/2017
+
+#include <cmath>    // pow, sqrt
+#include <fstream>  // read in matrices from file
+#include <iomanip>  // setprecision
+#include <iostream> // std io
+#include <stdlib.h> // c style exit
+
+using namespace std;
+
+// Cartesian class which defines addition, subtraction, multiplication and
+// iostream
+// behaviour
+class Cartesian // Cartesian class
+{
+  // friend functions to overload input and output operator
+  friend ostream &operator<<(ostream &os, Cartesian &vect1);  // ostreams
+  friend istream &operator>>(istream &ins, Cartesian &vect1); // istreams
+
+private:
+  // member data
+  int dimensions;  // number of dimensions
+  double *data{0}; // pointer to array where Cartesian elements are stored
+
+public:
+  // default constructor
+  Cartesian();
+  // parametrised constructor
+  Cartesian(const int &dim);
+  // destructor - free up memory
+  ~Cartesian() { delete[] data; }
+  // copy constructor for deep copy
+  Cartesian(const Cartesian &vect);
+  // move constructor
+  Cartesian(Cartesian &&vect1);
+  // assignment operator for deep copy
+  Cartesian &operator=(const Cartesian &vect1);
+  // move assignment operator
+  Cartesian &operator=(Cartesian &&vect1);
+
+  // accessors
+  int get_dimensions() const { return dimensions; } // no of dimensions
+  double get_element(const int &i) const { return data[i]; };
+
+  // modifiers
+  void set_element(const int &i, const double &value);
+
+  // overloaded operators
+  Cartesian operator+(const Cartesian &vect1) const; // addition
+  Cartesian operator-(const Cartesian &vect1) const; // subtraction
+  Cartesian operator*(const Cartesian &vect1) const; // multiplication
+  double &operator[](const int i) { return data[i]; }
+};
+// end of class
+
+// default constructor
+Cartesian::Cartesian() : dimensions{3} {
+  // create 3d vector and fill with zeroes
+  data = new double[dimensions];
+  for (int i = 0; i < dimensions; i++) {
+    data[i] = 0;
+  }
+}
+
+// parametrised constructor
+Cartesian::Cartesian(const int &dim) : dimensions{dim} {
+  // create 3d vector and fill with zeroes
+  data = new double[dimensions];
+  for (int i = 0; i < dimensions; i++) {
+    data[i] = 0;
+  }
+}
+
+// define ostream behaviour
+ostream &operator<<(ostream &os, Cartesian &vect1) {
+  os << "(";
+  for (int i{0}; i < vect1.get_dimensions(); i++) {
+    if (i == vect1.get_dimensions() - 1) {
+      os << vect1[i] << ")";
+    } else {
+      os << vect1[i] << ", ";
+    }
+  }
+  return os;
+}
+
+istream &operator>>(istream &is, Cartesian &vect1) {
+  for (int i{0}; i < vect1.get_dimensions(); i++) {
+    is >> vect1[i];
+    is.ignore(); // ignore the space
+  }
+  return is;
+}
+
+int main() {
+  //------------------------------
+  //---show off cartesian class---
+  //------------------------------
+  // default constructor
+  Cartesian v1;
+  cout << "Default constructor: v1 = " << v1 << endl;
+
+  // parametrised constructor
+  Cartesian v2{4};
+  cout << "Parametrised constructor: v2 = " << v2 << endl;
+  cout << "Enter a 4d vector to fill v2 (comma separated): ";
+  cin >> v2;
+  cout << "v2 = " << v2 << endl;
+
+  return 0;
+}
