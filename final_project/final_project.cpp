ea2676876977e91720eb37dda237ec3fe95e4ae4
Dónal Murray <dmurray654@gmail.com>
2017-03-22 20:41:28 +0000

    Add final project file

---
+++ final_project/final_project.cpp
@@ -0,0 +1,13 @@
+// final_project.cpp: AC Circuit Manipulator
+//
+// Author:  Dónal Murray
+// Date:    22/03/2017
+
+#include <iostream> // std io
+
+using namespace std;
+
+int main() {
+  cout << "Abandon all hope, ye who enter here.\n";
+  return 0;
+}
