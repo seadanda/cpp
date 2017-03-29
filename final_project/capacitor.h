079287f533eeeda4d74b5e9937d3c0a14d5134fb
Dónal Murray <dmurray654@gmail.com>
2017-03-29 16:32:22 +0100

    Add header guards and info

---
+++ final_project/capacitor.h
@@ -0,0 +1,11 @@
+/* capacitor.h
+ * Interface for Capacitor class to store and manipulate capacitors
+ *  Implementation:  capacitor.cpp
+ *  Author:          Dónal Murray
+ *  Date:            29/03/17
+ */
+
+#ifndef CAPACITOR_H
+#define CAPACITOR_H
+
+#endif
