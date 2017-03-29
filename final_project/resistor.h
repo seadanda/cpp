c8e5e53be09f047b53217fb3c84157ee54f0f5ce
Dónal Murray <dmurray654@gmail.com>
2017-03-29 16:41:07 +0100

    Add header guards and info

---
+++ final_project/resistor.h
@@ -0,0 +1,12 @@
+/* resistor.h
+ * Interface for Resistor class (derived from component class) to store and
+ * manipulate resistors
+ *  Implementation:  resistor.cpp
+ *  Author:          Dónal Murray
+ *  Date:            29/03/17
+ */
+
+#ifndef RESISTOR_H
+#define RESISTOR_H
+
+#endif
