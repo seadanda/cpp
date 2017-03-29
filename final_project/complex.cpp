0c40a747d4dbeab7ce4619b3cf35ac137e920fc8
Dónal Murray <dmurray654@gmail.com>
2017-03-29 16:33:10 +0100

    Add complex class from week5 assignment

---
+++ final_project/complex.cpp
@@ -0,0 +1,131 @@
+/* complex.cpp
+ * Implementation of Complex class to store and manipulate capacitors
+ *  Interface:      complex.h
+ *  Author:         Dónal Murray
+ *  Date:           29/03/17
+ */
+
+#include <cmath>    // sqrt, pow, atan2
+#include <iomanip>  // set precision
+#include <iostream> // std io
+
+#include "complex.h" // class interface
+
+// default constructor
+Complex::Complex() : real{0}, imaginary{0} {}
+
+// parametrised constructor
+Complex::Complex(double re, double im) : real{re}, imaginary{im} {}
+
+// destructor
+Complex::~Complex() {}
+
+// modifiers
+void Complex::set_real(const double re) { real = re; }
+void Complex::set_imaginary(const double im) { imaginary = im; }
+
+// accessors
+double Complex::get_real() const { return real; }
+double Complex::get_imaginary() const { return imaginary; }
+
+// member functions to return modulus and argument
+double Complex::modulus() const {
+  return sqrt(pow(real, 2) + pow(imaginary, 2));
+}
+double Complex::argument() const { return pow(atan2(imaginary, real), 2); }
+
+// function to calculate the complex conjugate of a complex number
+Complex Complex::conjugate() const // return complex conjugate
+{
+  Complex temp{real, imaginary}; // clone input complex number
+  temp.set_real(real);
+  if (imaginary != 0) {
+    // imaginary is not zero, invert the sign
+    temp.set_imaginary(-1 * imaginary);
+  }
+  return temp;
+}
+
+// define the addition of two complex numbers
+Complex Complex::operator+(const Complex &z2) const // return sum
+{
+  Complex tempz{real + z2.real, imaginary + z2.imaginary};
+  return tempz;
+}
+
+// define the subtraction of two complex numbers
+Complex Complex::operator-(const Complex &z2) const // return difference
+{
+  Complex temp{real - z2.real, imaginary - z2.imaginary};
+  return temp;
+}
+
+// define the product of two complex numbers
+Complex Complex::operator*(const Complex &z2) const // return product
+{
+  // x*y = (Re{x}*Re{y} - Im{x}*Im{y}) + i(Re{x}*Im{y}+Im{x}*Im{y})
+  Complex temp{real * z2.real - imaginary * z2.imaginary,
+               real * z2.imaginary + imaginary * z2.real};
+  return temp;
+}
+
+// define the quotient of two complex numbers
+Complex Complex::operator/(const Complex &z2) const // return quotient
+{
+  // x/y = x * conjugate(y) / modulus(y)
+  double denom{z2.modulus()}; // denominator
+  double re{(real * z2.real + imaginary * z2.imaginary) / denom};
+  double im{(imaginary * z2.real - real * z2.imaginary) / denom};
+  Complex temp{re, im};
+  return temp;
+}
+
+// define how complex numbers are inserted into an ostream
+ostream &operator<<(ostream &outs, const Complex &z) // define ostream behaviour
+{
+  if (z.imaginary < 0) {
+    // negative, minus already included in double
+    outs << z.real << z.imaginary << "i";
+  } else {
+    // positive or zero, insert + sign
+    outs << z.real << "+" << z.imaginary << "i";
+  }
+  return outs;
+}
+
+// define how complex numbers are read from an istream
+istream &operator>>(istream &ins, Complex &z) // define istream behaviour
+{
+  double re; // real part
+  double im; // imaginary part
+  char sign; // sign of imaginary part (+/-)
+
+  ins >> re   // read first number
+      >> sign // read sign of imaginary part
+      >> im;  // read second number
+  if (ins.fail()) {
+    // i is before the imaginary part, ignore it and read in the imaginary part
+    ins.clear();  // clear the fail bit
+    ins.ignore(); // ignore the i
+    ins >> im;    // read second number
+  } else {
+    // i is after imaginary part. ignore it to clear the stream
+    ins.ignore();
+  }
+
+  // a+ib a-ib
+  // a+bi a-bi
+  // set value of real part
+  z.set_real(re);
+
+  // set value of imaginary part depending on the sign
+  if ((sign == '+') || (im == 0)) {
+    // imaginary part is positive or zero
+    z.set_imaginary(im);
+  } else {
+    // imaginary part is negative
+    z.set_imaginary(-1 * im);
+  }
+
+  return ins;
+}
---
+++ final_project/complex.h
@@ -0,0 +1,52 @@
+/* complex.h
+ * Interface for Complex class to store and manipulate complex numbers
+ *  Implementation: complex.cpp
+ *  Author:         Dónal Murray
+ *  Date:           29/03/17
+ */
+
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+using namespace std;
+
+class Complex // class for complex numbers
+{
+  // friend functions to overload << and >> to define iostream behaviour
+  friend ostream &operator<<(ostream &outs, const Complex &z); // ostreams
+  friend istream &operator>>(istream &ins, Complex &z);        // istreams
+
+private:
+  // member data
+  double real;      // real part
+  double imaginary; // imaginary part
+
+public:
+  // default constructor
+  Complex();
+  // parametrised constructor
+  Complex(double re, double im);
+  // destructor
+  ~Complex();
+
+  // modifiers
+  void set_real(const double re);
+  void set_imaginary(const double im);
+
+  // accessors
+  double get_real() const;
+  double get_imaginary() const;
+
+  // member functions
+  double modulus() const;    // return modulus
+  double argument() const;   // return argument
+  Complex conjugate() const; // return conjugate
+
+  // overload +-*/ operators
+  Complex operator+(const Complex &z2) const;
+  Complex operator-(const Complex &z2) const;
+  Complex operator*(const Complex &z2) const;
+  Complex operator/(const Complex &z2) const;
+};
+
+#endif
