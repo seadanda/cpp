a99051723c678f1e4fc75dfd9c126cdb4d72d6ff
Dónal Murray <dmurray654@gmail.com>
2017-03-29 16:40:38 +0100

    Add header guards and info

---
+++ final_project/circuit.h
@@ -0,0 +1,11 @@
+/* circuit.h
+ * Interface for Circuit class to store and manipulate circuits
+ *  Implementation:  circuit.cpp
+ *  Author:          Dónal Murray
+ *  Date:            29/03/17
+ */
+
+#ifndef CIRCUIT_H
+#define CIRCUIT_H
+
+#endif
