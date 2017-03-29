57fb9f90e1c5799ee88a5b1503956d5756506b08
Dónal Murray <dmurray654@gmail.com>
2017-03-29 15:37:49 +0100

    Change name of complex class

--- week5/week5.cpp
+++ week5/week5.cpp
@@ -8,11 +8,11 @@
 
 using namespace std;
 
-class complx // class for complex numbers
+class Complex // class for complex numbers
 {
   // friends of the class
-  friend ostream &operator<<(ostream &outs, const complx &z);
-  friend complx operator>>(istream &ins, complx &z);
+  friend ostream &operator<<(ostream &outs, const Complex &z);
+  friend Complex operator>>(istream &ins, Complex &z);
 
 private:
   // member data
@@ -21,11 +21,11 @@ private:
 
 public:
   // default constructor
-  complx() : real{0}, imaginary{0} {}
+  Complex() : real{0}, imaginary{0} {}
   // parametrised constructor
-  complx(double re, double im) : real{re}, imaginary{im} {}
+  Complex(double re, double im) : real{re}, imaginary{im} {}
   // destructor
-  ~complx() {}
+  ~Complex() {}
 
   // modifiers
   void set_real(const double re) { real = re; }
@@ -40,21 +40,21 @@ public:
   double get_argument() const { return pow(atan2(imaginary, real), 2); }
 
   // member function to return complex conjugate
-  complx get_conjugate() const;
+  Complex get_conjugate() const;
 
   // member functions to overload + and - operators
-  complx operator+(const complx &z2) const;
-  complx operator-(const complx &z2) const;
+  Complex operator+(const Complex &z2) const;
+  Complex operator-(const Complex &z2) const;
 
   // member functions to overload * and / operators
-  complx operator*(const complx &z2) const;
-  complx operator/(const complx &z2) const;
+  Complex operator*(const Complex &z2) const;
+  Complex operator/(const Complex &z2) const;
 }; // end of class
 
 // function to return the complex conjugate of a complex number
-complx complx::get_conjugate() const // return complex conjugate
+Complex Complex::get_conjugate() const // return complex conjugate
 {
-  complx temp{real, imaginary}; // clone input complex number
+  Complex temp{real, imaginary}; // clone input complex number
   temp.set_real(real);
   if (imaginary != 0) {
     // imaginary is not zero, invert the sign
@@ -64,41 +64,41 @@ complx complx::get_conjugate() const // return complex conjugate
 }
 
 // define the addition of two complex numbers
-complx complx::operator+(const complx &z2) const // return sum
+Complex Complex::operator+(const Complex &z2) const // return sum
 {
-  complx tempz{real + z2.real, imaginary + z2.imaginary};
+  Complex tempz{real + z2.real, imaginary + z2.imaginary};
   return tempz;
 }
 
 // define the subtraction of two complex numbers
-complx complx::operator-(const complx &z2) const // return difference
+Complex Complex::operator-(const Complex &z2) const // return difference
 {
-  complx temp{real - z2.real, imaginary - z2.imaginary};
+  Complex temp{real - z2.real, imaginary - z2.imaginary};
   return temp;
 }
 
 // define the product of two complex numbers
-complx complx::operator*(const complx &z2) const // return product
+Complex Complex::operator*(const Complex &z2) const // return product
 {
   // x*y = (Re{x}*Re{y} - Im{x}*Im{y}) + i(Re{x}*Im{y}+Im{x}*Im{y})
-  complx temp{real * z2.real - imaginary * z2.imaginary,
-              real * z2.imaginary + imaginary * z2.real};
+  Complex temp{real * z2.real - imaginary * z2.imaginary,
+               real * z2.imaginary + imaginary * z2.real};
   return temp;
 }
 
 // define the quotient of two complex numbers
-complx complx::operator/(const complx &z2) const // return quotient
+Complex Complex::operator/(const Complex &z2) const // return quotient
 {
   // x/y = x * conjugate(y) / modulus(y)
   double denom{z2.get_modulus()}; // denominator
   double re{(real * z2.real + imaginary * z2.imaginary) / denom};
   double im{(imaginary * z2.real - real * z2.imaginary) / denom};
-  complx temp{re, im};
+  Complex temp{re, im};
   return temp;
 }
 
 // define how complex numbers are inserted into an ostream
-ostream &operator<<(ostream &outs, const complx &z) // define ostream behaviour
+ostream &operator<<(ostream &outs, const Complex &z) // define ostream behaviour
 {
   if (z.imaginary < 0) {
     // negative, minus already included in double
@@ -110,7 +110,7 @@ ostream &operator<<(ostream &outs, const complx &z) // define ostream behaviour
 }
 
 // define how complex numbers are read from an istream
-complx operator>>(istream &ins, complx &z) // define istream behaviour
+Complex operator>>(istream &ins, Complex &z) // define istream behaviour
 {
   double re; // real part
   double im; // imaginary part
@@ -147,11 +147,11 @@ complx operator>>(istream &ins, complx &z) // define istream behaviour
 int main() // main
 {
   // declare two complex numbers using the parametrised constructor
-  complx z1(3, 4);
-  complx z2(1, -2);
+  Complex z1(3, 4);
+  Complex z2(1, -2);
 
   // push complex numbers onto vector of zs
-  vector<complx *> zs;
+  vector<Complex *> zs;
   zs.push_back(&z1);
   zs.push_back(&z2);
 
@@ -164,8 +164,8 @@ int main() // main
        << "    z1 / z2 = " << setprecision(3) << (z1 / z2) << endl;
 
   // read in two new complex numbers and do it all again
-  complx z3;
-  complx z4;
+  Complex z3;
+  Complex z4;
   cout << "Enter two more complex numbers and watch the magic happen...\nz3 = ";
   cin >> z3;
   cout << "z4 = ";
