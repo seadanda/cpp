4e8edfa83ce47458d63093ec928addf2bc4791ed
Dónal Murray <dmurray654@gmail.com>
2017-03-29 16:40:57 +0100

    Add header guards and info

---
+++ final_project/inductor.h
@@ -0,0 +1,12 @@
+/* inductor.h
+ * Interface for Inductor class (derived from component class) to store and
+ * manipulate inductors
+ *  Implementation:  inductor.cpp
+ *  Author:          Dónal Murray
+ *  Date:            29/03/17
+ */
+
+#ifndef INDUCTOR_H
+#define INDUCTOR_H
+
+#endif
