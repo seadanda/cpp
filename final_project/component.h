0710bf8eec326920f50ef8d759bbfd62aee6c74a
Dónal Murray <dmurray654@gmail.com>
2017-03-29 16:40:17 +0100

    Add header guards and info

---
+++ final_project/component.h
@@ -0,0 +1,11 @@
+/* component.h
+ * Interface for Component abstract base class
+ *  Implementation:  component.cpp
+ *  Author:          Dónal Murray
+ *  Date:            29/03/17
+ */
+
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#endif
