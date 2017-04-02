5529f458dea8e362f0798c60fb92df86f2fba339
Dónal Murray <dmurray654@gmail.com>
2017-04-02 17:54:58 +0100

    Add interface for component class

--- final_project/component.h
+++ final_project/component.h
@@ -8,6 +8,38 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
-class Component {};
+#include "complex.h"
+
+class Component {
+protected:
+  double frequency;        // frequency of circuit
+  Complex impedence;       // impedence of component
+  double phase_difference; // phase difference of impedence
+
+public:
+  // constructors and virtual destructor
+  Component();
+  Component(const double &, const Complex &, const double &);
+  virtual ~Component();
+
+  // modifiers
+  // set frequency of component
+  virtual void set_frequency(const double &) = 0;
+  // set impedence of component
+  virtual void set_impedence(const Complex &) = 0;
+  // set phase difference of component
+  virtual void set_phase_difference(const double &) = 0;
+
+  // accessors
+  // return frequency of component
+  virtual double get_frequency() const = 0;
+  // return impedence of component
+  virtual Complex get_impedance() const = 0;
+  // return phase difference of component
+  virtual double get_phase_difference() const = 0;
+
+  // calculate the magnitude of the components impedence
+  virtual double get_mag_impedance() const = 0;
+};
 
 #endif
