eda25ce5b8273f357336733e9c56af232d58716c
Dónal Murray <dmurray654@gmail.com>
2017-04-02 18:29:30 +0100

    Add abstract base class implementation. move destructor to interface instead of implementation

--- final_project/component.cpp
+++ final_project/component.cpp
@@ -5,3 +5,13 @@
  *  Author:         Dónal Murray
  *  Date:           29/03/17
  */
+
+#include "component.h"
+#include "complex.h"
+
+// default constructor
+Component::Component() : frequency{0}, impedence{0}, phase_difference{0} {}
+
+// parametrised constructor
+Component::Component(const double &freq, const Complex &Z, const double &phase)
+    : frequency{freq}, impedence{Z}, phase_difference{phase} {}
