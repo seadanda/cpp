a3be29461593798d8f8f064071bcabdcc3e879d2
Dónal Murray <dmurray654@gmail.com>
2017-05-07 23:52:17 +0100

    Reorder code for neatness

--- final_project/main.cpp
+++ final_project/main.cpp
@@ -19,7 +19,9 @@
 
 using namespace std;
 
-// main function
+//-----------------------------------------------------------------------------
+//---main function
+//-----------------------------------------------------------------------------
 int main() {
   // call main menu function
   main_menu();
@@ -30,7 +32,61 @@ int main() {
   return 0;
 }
 
-//---functions---
+//-----------------------------------------------------------------------------
+//---functions
+//-----------------------------------------------------------------------------
+
+//---housekeeping
+// exception handling
+void error(const int &err) {
+  cerr << "Error: ";
+  switch (err) {
+  case 1:
+    cerr << "not a valid input.\n";
+    break;
+  default:
+    cerr << "an error occurred\n";
+    break;
+  }
+}
+
+// function to take input and make sure it is an allowed value (in parameters)
+template <class T> T take_input(initializer_list<T> allowed) {
+  T temp;
+  cin >> temp; // take input
+  while (cin.fail() || [&]() {
+    // lambda to check for disallowed inputs not caught by a fail flag
+    bool valid_input{true};
+    for (auto it : allowed) {
+      // iterate through input parameters and check that the input is allowed
+      if (temp == it) {
+        // if the input is in the list, return false
+        valid_input = false;
+      }
+    }
+    return valid_input;
+  }()) {
+    // clear fail bit and flush stream buffer
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    // take input again
+    cerr << "Please choose a valid option: ";
+    cin >> temp;
+  }
+  // clear rest of stream, in case there is anything else there
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  return temp;
+}
+
+// template function to clean up memory at end of program
+template <class T> void clean_up(vector<T *> &lib) {
+  for (auto it = lib.begin(); it != lib.end(); it++) {
+    delete *it;
+  }
+  lib.clear();
+}
+
+//---menu screens
 // main menu
 void main_menu() {
   int main_choice;
@@ -69,66 +125,16 @@ void main_menu() {
   }
 }
 
-// function to add a component to the component library
-template <class T> void add_component() {
-  double temp_val;
-  // check the type of the component and ask for value
-  if (is_same<T, Resistor>::value) {
-    // type is resistor
-    cout << "Enter the resistance in \u03A9: ";
-  } else if (is_same<T, Capacitor>::value) {
-    // type is capacitor
-    cout << "Enter the capacitance in \u00B5F: ";
-  } else if (is_same<T, Inductor>::value) {
-    // type is inductor
-    cout << "Enter the inductance in \u00B5H: ";
-  }
-  cin >> temp_val;
-  libs::component_lib.push_back(new T{temp_val});
-}
-
-template <class T> void clean_up(vector<T *> &lib) {
-  for (auto it = lib.begin(); it != lib.end(); it++) {
-    delete *it;
-  }
-  lib.clear();
-}
-
-// function to take input and make sure it is an allowed value (in parameters)
-template <class T> T take_input(initializer_list<T> allowed) {
-  T temp;
-  cin >> temp; // take input
-  while (cin.fail() || [&]() {
-    // lambda to check for disallowed inputs not caught by a fail flag
-    bool valid_input{true};
-    for (auto it : allowed) {
-      // iterate through input parameters and check that the input is allowed
-      if (temp == it) {
-        // if the input is in the list, return false
-        valid_input = false;
-      }
-    }
-    return valid_input;
-  }()) {
-    // clear fail bit and flush stream buffer
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    // take input again
-    cerr << "Please choose a valid option: ";
-    cin >> temp;
-  }
-  // clear rest of stream, in case there is anything else there
-  cin.ignore(numeric_limits<streamsize>::max(), '\n');
-  return temp;
-}
-
 void comp_menu() {
-  using namespace libs;
+  using namespace libs; // component and circuit libraries
 
+  // bools for menu loops
   bool quit_comp{false};
+  bool quit_add;
+
   int comp_choice;
   while (!quit_comp) {
-    bool quit_add{false};
+    quit_add = false; // reset add component menu loop
     cout << "--------Component menu--------\n"
          << "Select an option:\n"
          << "1     Add components to library\n"
@@ -137,7 +143,7 @@ void comp_menu() {
          << "0     Back to main menu\n"
          << "-------------------------------\n"
          << "Option: ";
-    comp_choice = take_input({0, 1, 2, 3});
+    comp_choice = take_input({0, 1, 2, 3}); // 0, 1, 2, 3 only valid inputs
     switch (comp_choice) {
     case 0:
       quit_comp = true;
@@ -174,6 +180,36 @@ void comp_menu() {
       print_component_lib();
       break;
     }
+  }
+}
+
+// template function to add a component to the component library
+template <class T> void add_component() {
+  double temp_val;
+  // check the type of the component and ask for value
+  if (is_same<T, Resistor>::value) {
+    // type is resistor
+    cout << "Enter the resistance in \u03A9: ";
+  } else if (is_same<T, Capacitor>::value) {
+    // type is capacitor
+    cout << "Enter the capacitance in \u00B5F: ";
+  } else if (is_same<T, Inductor>::value) {
+    // type is inductor
+    cout << "Enter the inductance in \u00B5H: ";
+  }
+  cin >> temp_val;
+  libs::component_lib.push_back(new T{temp_val});
+}
+
+// function to iterate through component library and and print the components
+void print_component_lib() {
+  using namespace libs;
+  cout << "-------Component Library-------\n"
+       << "| Label   Component  Value    |\n"
+       << "-------------------------------\n";
+  for (auto it = component_lib.begin(); it != component_lib.end(); it++) {
+    // print out each component's label, type and value
+    cout << **it << endl;
   }
 }
 
@@ -216,17 +252,8 @@ void circ_menu() {
   }
 }
 
-void error(const int &err) {
-  cerr << "Error: ";
-  switch (err) {
-  case 1:
-    cerr << "not a valid input.\n";
-    break;
-  default:
-    cerr << "an error occurred\n";
-    break;
-  }
-}
+// function to create a circuit
+void create_circuit() { cout << "--------Circuit Creator--------\n"; }
 
 void print_circuit_lib() {
   using namespace libs;
@@ -238,18 +265,3 @@ void print_circuit_lib() {
     cout << **it << endl;
   }
 }
-
-// function to iterate through component library and and print the components
-void print_component_lib() {
-  using namespace libs;
-  cout << "-------Component Library-------\n"
-       << "| Label   Component  Value    |\n"
-       << "-------------------------------\n";
-  for (auto it = component_lib.begin(); it != component_lib.end(); it++) {
-    // print out each component's label, type and value
-    cout << **it << endl;
-  }
-}
-
-// function to create a circuit
-void create_circuit() { cout << "--------Circuit Creator--------\n"; }
